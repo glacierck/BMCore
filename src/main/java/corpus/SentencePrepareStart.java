bcfbeaca6ac9c07c90cc156a9b2a235839a64fc4
ck <glacierck@126.com>
2016-10-31 18:20:45 +0800

    1

--- src/main/java/corpus/SentencePrepareStart.java
+++ src/main/java/corpus/SentencePrepareStart.java
@@ -2,7 +2,7 @@ package corpus;
 
 import cn.edu.fudan.corpus.SentencePrepare;
 /**
- * 一、产生句子分类模型训练所需要的文件：包括正负样本的文件（顺序随机打乱）；字符表；标签文件
+ * 一、【训练样本准备】产生句子分类模型训练所需要的文件：包括正负样本的文件（顺序随机打乱）；字符表；标签文件
 * */
 public class SentencePrepareStart {
 
@@ -10,9 +10,9 @@ public class SentencePrepareStart {
 		// TODO Auto-generated method stub
 		String preprocessFile = "conf/Preprocess.properties";
 		String posTaggerFile = "conf/PosTagger.properties";
-		String sentenceCorpusFile = "sentence/sample_sentence.utf8";
-		String negativeCorpusFile = "sentence/sample_negative.utf8";
-		String corpusFile = "sentence/sentence_corpus.utf8";//指定训练样本的文件路径。
+		String sentenceCorpusFile = "sentence/sample_sentence.utf8";//正样本
+		String negativeCorpusFile = "sentence/sample_negative.utf8";//负样本,指应用不感兴趣的句子。
+		String corpusFile = "sentence/sentence_corpus.utf8";//指定训练样本的文件路径。(中文命名识别任务所准备的训练样本文件)
 		String vocaburaryFile = "sentence/token.utf8";//指定训练样本中出现字符的字符表。
 		String labelFile = "sentence/classificationLabel.utf8";//指定任务标签集合的文件路径.
 
